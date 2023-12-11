9985ace357bf022523b69792b8cb040f6e48167c
“samiur-r” <samiurrahman.dev@gmail.com>
2023-12-11 22:15:08 +0600

    chore: fix accessibility issue

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -11,7 +11,7 @@ const SectionHeader: React.FC<SectionHeaderProps> = ({
 }) => {
   return (
     <div className="container md:mx-auto px-5 md:px-0 mb-4 md:mb-16">
-      <h6 className="text-xl md:text-3xl font-glancyr">{subHeadline}</h6>
+      <p className="text-xl md:text-3xl font-glancyr">{subHeadline}</p>
       <h2 className="font-glancyr font-bold text-3xl md:text-5xl max-w-2xl mt-5">
         {headline}
       </h2>
